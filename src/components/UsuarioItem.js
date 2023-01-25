29dd68834d60bdb9e9c1f667c50d696ee0783d0a
GuilhermeMaywald <guilhermemaywald@gmail.com>
2023-01-25 15:20:55 -0300

    Resolucao atividade 05

---
+++ src/components/UsuarioItem.js
@@ -0,0 +1,15 @@
+import './UsuarioItem.css'
+
+function UsuarioItem ({ nome, estado, onClickItem, indice }) {
+    return (
+        <>
+            <p
+                onClick={() => onClickItem(indice)}
+                className={estado ? 'itemAtivo' : ''}>
+                    {nome}
+            </p>
+        </>
+    )
+}
+
+export default UsuarioItem
